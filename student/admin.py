1001e4a19b526c4fa41c400b662fb734350b5f93
mkapiczy <mix.kap@gmail.com>
2016-11-07 21:28:21 +0100

    [Student] Adding Project reams and Student user to admin panel

--- student/admin.py
+++ student/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Album
 from .models import Song
+from .models import ProjectTeam, StudentUser
 
 admin.site.register(Album)
 admin.site.register(Song)
+admin.site.register(ProjectTeam)
+admin.site.register(StudentUser)
