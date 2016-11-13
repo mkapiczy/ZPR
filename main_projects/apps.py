c42fa00d01d767f306b3f41bf3e1d64a39a7fc77
mkapiczy <mix.kap@gmail.com>
2016-11-13 19:21:09 +0100

    Project structure - division on separetedjango apps [modules]

---
+++ main_projects/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MainProjectsConfig(AppConfig):
+    name = 'main_projects'
---
+++ tutor_posts/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TutorPostsConfig(AppConfig):
+    name = 'tutor_posts'
