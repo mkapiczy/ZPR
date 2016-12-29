1d1cff8f4818c86bf8b698dc5b662a3c5d612de9
Michał Kapiczyński <michal.kapiczynski@bms.com.pl>
2016-12-29 16:15:49 +0100

    [Tests] test_get_student_user_from_request

--- student/tests.py
+++ student/tests.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import User
+from django.test import Client
+from django.test import RequestFactory
 from django.test import TestCase
 
-# Create your tests here.
+from main.models import MyUser
+from main.views import createNewUserProfile, createNewStudentUser
+from student.methods import getStudentUserFromRequest
+
+
+class MainTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        self.user = User.objects.create_user(username='student', email='test@test', password='student')
+
+        studentUser = MyUser()
+        studentUser.username = self.user.username
+        studentUser.user = self.user
+        studentUser.password = self.user.password
+        studentUser.first_name = 'Student'
+        studentUser.last_name = 'Test'
+        studentUser.is_tutor = False
+        studentUser.save()
+
+        profile = createNewUserProfile(studentUser)
+        createNewStudentUser(profile)
+
+    def test_get_student_user_from_request(self):
+        client = Client()
+        client.login(username='student', password='student')
+        request = self.factory.get('/zpr/')
+        request.user = self.user
+        userFromRequest = getStudentUserFromRequest(request)
+        self.assertEqual(userFromRequest.profile.user.user, self.user)
