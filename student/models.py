47b90c715af542d1976752e4c0e0c7339bd1c1c3
Michał Kapiczyński <michal.kapiczynski@bms.com.pl>
2016-12-29 09:18:04 +0100

    [Model] - changing relation in student to projectTeams ans signedProjects and adding getters and setters to operate with that fields for specific course

--- student/models.py
+++ student/models.py
@@ -8,6 +8,7 @@ class ProjectTeam(models.Model):
     accepted = models.BooleanField(default=False)
     course = models.ForeignKey('main.Course', null=True)
 
+
 class StudentUser(models.Model):
     profile = models.ForeignKey('main.UserProfile')
     album_number = models.CharField(max_length=16)
@@ -15,8 +16,40 @@ class StudentUser(models.Model):
     status = models.BooleanField
     group = models.CharField(max_length=32)
     courses = models.ManyToManyField(Course)
-    project_teams = models.ManyToManyField(ProjectTeam, null=True)
-    signed_projects = models.ManyToManyField('main.Project', null=True)
+    projectTeams = models.ManyToManyField(ProjectTeam, null=True)
+    signedProjects = models.ManyToManyField('main.Project', null=True)
 
     class Meta:
         db_table = 'student_user'
+
+    def getCourseById(self, courseId):
+        for course in self.courses.all():
+            if course.id == courseId:
+                return course
+        return None
+
+    def getSignedProjectForCourse(self, courseId):
+        for project in self.signedProjects.all():
+            if project.course.id == courseId:
+                return project
+        return None
+
+    def setSignedProjectForCourse(self, project, courseId):
+        alreadySignedCourse = self.getSignedProjectForCourse(courseId)
+        if alreadySignedCourse is not None:
+            self.signedProjects.remove(alreadySignedCourse)
+        self.signedProjects.add(project)
+        self.save()
+
+    def getProjectTeamForCourse(self, courseId):
+        for team in self.projectTeams.all():
+            if team.course.id == courseId:
+                return team
+        return None
+
+    def setProjectTeamForCourse(self, projectTeam, courseId):
+        alreadyAssignedProjectTeam = self.getProjectTeamForCourse(courseId)
+        if alreadyAssignedProjectTeam is not None:
+            self.projectTeams.remove(alreadyAssignedProjectTeam)
+        self.projectTeams.add(projectTeam)
+        self.save()
