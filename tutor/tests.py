4fcd0cf63ebfa15f6910b07389c9d2563f0af389
Michał Kapiczyński <michal.kapiczynski@bms.com.pl>
2016-12-30 11:06:41 +0100

    [Tests] TutorUserTestCase - getTutorUserFromRequest test

--- tutor/tests.py
+++ tutor/tests.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import User
+from django.test import Client
+from django.test import RequestFactory
 from django.test import TestCase
 
-# Create your tests here.
+from main.methods import createNewTutorUser
+from main.models import MyUser
+from main.views import createNewUserProfile
+from tutor.methods import getTutorUserFromRequest
+
+
+class TutorUserTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+        self.user = User.objects.create_user(username='tutor', email='test@test', password='tutor')
+
+        myUser = MyUser()
+        myUser.username = self.user.username
+        myUser.user = self.user
+        myUser.password = self.user.password
+        myUser.first_name = 'Tutor'
+        myUser.last_name = 'Test'
+        myUser.is_tutor = True
+        myUser.save()
+
+        profile = createNewUserProfile(myUser)
+        createNewTutorUser(profile)
+
+    def test_get_tutor_user_from_request(self):
+        client = Client()
+        client.login(username='tutor', password='tutor')
+        request = self.factory.get('/zpr/')
+        request.user = self.user
+        userFromRequest = getTutorUserFromRequest(request)
+        self.assertEqual(userFromRequest.profile.user.user, self.user)
